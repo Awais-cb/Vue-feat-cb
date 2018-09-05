222adfe182909fbc474316d42bde1aea576bbab6
Awais-cb <awaisfiazit@gmail.com>
2018-09-05 19:50:41 +0500

    added links to videos

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,13 +3,15 @@
     <h3 class="border-bottom">{{ $t("lang.featured_videos") }}</h3>
     <div v-if="featuredVideos.length" class="row">
   		<div v-for='obj in featuredVideos' class="col-xs-4 col-sm-4 col-lg-4 col-md-4" >
-  			<div class="card" style="width: 18rem;">	
-  				<img class="card-img-top" v-bind:src=obj.video.thumbs.big alt="Card image cap">
-  			 	<div class="card-body">
-  				    <h6 class="card-title">{{obj.video.title}}</h6>
-  				    <p class="card-text">{{obj.video.views}} {{$t('lang.views')}}</p>
-  			  	</div>
-  			</div>
+  			<router-link class="nav-link" to="/">
+          <div class="card" style="width: 18rem;">	
+    				<img class="card-img-top" v-bind:src=obj.video.thumbs.big alt="Card image cap">
+    			 	<div class="card-body">
+    				    <h6 class="card-title">{{obj.video.title}}</h6>
+    				    <p class="card-text">{{obj.video.views}} {{$t('lang.views')}}</p>
+    			  	</div>
+    			</div>
+        </router-link>
   		</div>
 	  </div>
     <div v-else class="row">
@@ -20,13 +22,15 @@
   <h3 class="border-bottom">{{ $t("lang.recent_videos") }}</h3>
 	<div v-if="recVideos.length" class="row">
 		<div v-for='obj in recVideos' class="col-xs-4 col-sm-4 col-lg-4 col-md-4" >
-			<div class="card" style="width: 18rem;">	
-				<img class="card-img-top" v-bind:src=obj.video.thumbs.big alt="Card image cap">
-			 	<div class="card-body">
-				    <h6 class="card-title">{{obj.video.title}}</h6>
-				    <p class="card-text">{{obj.video.views}} {{$t('lang.views')}}</p>
-			  	</div>
-			</div>
+			<router-link class="nav-link" to="/">
+        <div class="card" style="width: 18rem;">	
+  				<img class="card-img-top" v-bind:src=obj.video.thumbs.big alt="Card image cap">
+  			 	<div class="card-body">
+  				    <h6 class="card-title">{{obj.video.title}}</h6>
+  				    <p class="card-text">{{obj.video.views}} {{$t('lang.views')}}</p>
+  			  	</div>
+  			</div>
+      </router-link>
 		</div>
 	</div>
   <div v-else class="row">
--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -3,13 +3,15 @@
 	    <h3 class="border-bottom">{{$t('lang.videos')}}</h3>
 	    <div v-if='videos.length' class="row">
   			<div v-for='obj in videos' class="col-xs-4 col-sm-4 col-lg-4 col-md-4" >
-  				<div class="card" style="width: 18rem;">	
-  					<img class="card-img-top" v-bind:src=obj.video.thumbs.big alt="Card image cap">
-  				 	<div class="card-body">
-  					    <h6 class="card-title">{{obj.video.title}}</h6>
-  					    <p class="card-text">{{obj.video.views}} {{$t('lang.views')}}</p>
-  				  	</div>
-  				</div>
+  				<router-link class="nav-link" to="/">
+            <div class="card" style="width: 18rem;">	
+    					<img class="card-img-top" v-bind:src=obj.video.thumbs.big alt="Card image cap">
+    				 	<div class="card-body">
+    					    <h6 class="card-title">{{obj.video.title}}</h6>
+    					    <p class="card-text">{{obj.video.views}} {{$t('lang.views')}}</p>
+    				  	</div>
+    				</div>
+          </router-link>
   			</div>
 		  </div>
       <div v-else class="row">
